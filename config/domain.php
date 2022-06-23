853ba874b2918a52c0696882361106e3c9891a48
Marcel Stadelmann <m@marceli.to>
2022-06-23 11:07:20 +0200

    add multidomain

---
+++ config/domain.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+  'env_stub' => '.env',
+  'storage_dirs' => [
+    'app' => [
+      'public' => [
+      ],
+    ],
+    'framework' => [
+      'cache' => [
+      ],
+      'testing' => [
+      ],
+      'sessions' => [
+      ],
+      'views' => [
+      ],
+    ],
+    'logs' => [
+    ],
+  ],
+  'domains' => [
+    'eglistrasse.aporta-stiftung.ch.local' => 'eglistrasse.aporta-stiftung.ch.local',
+  ],
+ ];
--- routes/web.php
+++ routes/web.php
@@ -16,33 +16,42 @@ use App\Http\Controllers\TestController;
 Auth::routes(['verify' => true, 'register' => false]);
 Route::get('/logout', 'Auth\LoginController@logout');
 
+
+// Frontend Routes
 Route::get('/angebot/{collection:uuid}/detail/{collectionItem:uuid}', [CollectionController::class, 'show'])->name('offer.show');
 Route::get('/angebot/{collection:uuid}/{hash?}', [CollectionController::class, 'show'])->name('offer.list');
 
-Route::get('/', [PageController::class, 'index'])->name('home');
+if (
+  \App::domain() == 'liegenschaften.aporta-stiftung.ch.local' || 
+  \App::domain() == 'liegenschaften.aporta-stiftung.ch.marceli.to' || 
+  \App::domain() == 'liegenschaften.aporta-stiftung.ch'
+)
+{
+  Route::get('/', [PageController::class, 'index'])->name('home');
 
-// Routes for testing
-Route::get('/notify', [TestController::class, 'notify']);
-Route::get('/tenants', [TestController::class, 'tenants']);
-Route::get('/tenant/{tenant}', [TestController::class, 'tenant']);
-Route::get('/apartments', [TestController::class, 'apartments']);
-Route::get('/apartments/uuid', [TestController::class, 'apartmentUuid']);
-Route::get('/apartment/{apartment}', [TestController::class, 'apartment']);
-Route::get('/buildings/{estate}', [TestController::class, 'buildings']);
-Route::get('/building/{building}', [TestController::class, 'building']);
-Route::get('/estates', [TestController::class, 'estates']);
-Route::get('/estate/{estate}', [TestController::class, 'estate']);
-Route::get('/floors/{estate}', [TestController::class, 'floors']);
-Route::get('/rooms/{estate}', [TestController::class, 'rooms']);
-Route::get('/buildings/{estate}', [TestController::class, 'buildings']);
-Route::get('/mail/queue', [TestController::class, 'mailQueue']);
-
-
-// Logged in users
-Route::middleware('auth:sanctum', 'verified')->group(function() {
-  Route::get('/administration/{any?}', function () {
-    return view('layout.authenticated');
-  })->where('any', '.*')->middleware('role:admin')->name('applications');
-});
+  // Routes for testing
+  Route::get('/notify', [TestController::class, 'notify']);
+  Route::get('/tenants', [TestController::class, 'tenants']);
+  Route::get('/tenant/{tenant}', [TestController::class, 'tenant']);
+  Route::get('/apartments', [TestController::class, 'apartments']);
+  Route::get('/apartments/uuid', [TestController::class, 'apartmentUuid']);
+  Route::get('/apartment/{apartment}', [TestController::class, 'apartment']);
+  Route::get('/buildings/{estate}', [TestController::class, 'buildings']);
+  Route::get('/building/{building}', [TestController::class, 'building']);
+  Route::get('/estates', [TestController::class, 'estates']);
+  Route::get('/estate/{estate}', [TestController::class, 'estate']);
+  Route::get('/floors/{estate}', [TestController::class, 'floors']);
+  Route::get('/rooms/{estate}', [TestController::class, 'rooms']);
+  Route::get('/buildings/{estate}', [TestController::class, 'buildings']);
+  Route::get('/mail/queue', [TestController::class, 'mailQueue']);
+    
+  // Logged in users
+  Route::middleware('auth:sanctum', 'verified')->group(function() {
+    Route::get('/administration/{any?}', function () {
+      return view('layout.authenticated');
+    })->where('any', '.*')->middleware('role:admin')->name('applications');
+  });
+  
+}
 
 
