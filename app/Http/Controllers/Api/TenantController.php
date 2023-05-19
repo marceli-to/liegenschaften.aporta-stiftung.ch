915c684ca378e9808e9276c8f5de12481496fcc2
Marcel Stadelmann <m@marceli.to>
2023-05-19 17:35:14 +0200

    wip

--- app/Http/Controllers/Api/TenantController.php
+++ app/Http/Controllers/Api/TenantController.php
@@ -16,13 +16,18 @@ class TenantController extends Controller
   public function get($searchTerm = NULL)
   { 
     // If search term is provided, filter by name, firstnam, email or phone
+    // extend search for apartment.building.street
+
     if ($searchTerm)
     {
       $data = Tenant::with('apartment.room', 'apartment.floor', 'apartment.building')->whereHas('apartment')->where(function($query) use ($searchTerm) {
         $query->where('name', 'LIKE', "%{$searchTerm}%")
           ->orWhere('firstname', 'LIKE', "%{$searchTerm}%")
           ->orWhere('email', 'LIKE', "%{$searchTerm}%")
-          ->orWhere('phone', 'LIKE', "%{$searchTerm}%");
+          ->orWhere('phone', 'LIKE', "%{$searchTerm}%")
+          ->orWhereHas('apartment.building', function($query) use ($searchTerm) {
+            $query->where('street', 'LIKE', "%{$searchTerm}%");
+          });
       })->orderBy('name')->get();
     }
     else
